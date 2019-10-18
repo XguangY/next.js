0b4069017eedec1b1f5f015b1a5635030e4b32b1
theone <xuguangyue@dtrobo.com>
2019-10-18 17:31:50 +0800

    生命周期钩子

--- next-create/pages/index.js
+++ next-create/pages/index.js
@@ -1,34 +1,98 @@
+// import React from 'react'
+// import Link from "next/link";
+// import Router from 'next/router'
+// const Home = () => {
+//   function goXu1() {
+//     Router.push('/xu1')
+//   }
+//   function goXU() {
+//     Router.push({
+//       pathname: '/XUXU',
+//       query: {
+//         name: '小小徐'
+//       }
+//     })
+//   }
+//   return (
+//     <div>
+//       <div>首页</div>
+//       <Link href="/xu1"><a>xu1</a></Link>
+//       <Link href="/xu2"><a>xu2</a></Link>
+//       <div>
+//         <button onClick={goXu1}>去xu1</button>
+//       </div>
+//       <Link href="/XUXU?name=老徐"><a>老徐过去</a></Link>
+//       <br/>
+//       <Link href="/XUXU?name=小徐"><a>小徐过去</a></Link>
+//       <div>
+//         <button onClick={goXU}>小小徐过去</button>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Home
+
 import React from 'react'
-import Link from "next/link";
+import Link from 'next/link'
 import Router from 'next/router'
+
+
 const Home = () => {
-  function goXu1() {
-    Router.push('/xu1')
-  }
-  function goXU() {
+
+  function goXUXU(){
     Router.push({
-      pathname: '/XUXU',
-      query: {
-        name: '小小徐'
+      pathname:'/XUXU',
+      query:{
+        name:'井空'
       }
     })
   }
-  return (
-    <div>
-      <div>首页</div>
-      <Link href="/xu1"><a>xu1</a></Link>
-      <Link href="/xu2"><a>xu2</a></Link>
-      <div>
-        <button onClick={goXu1}>去xu1</button>
-      </div>
-      <Link href="/XUXU?name=老徐"><a>老徐过去</a></Link>
-      <br/>
-      <Link href="/XUXU?name=小徐"><a>小徐过去</a></Link>
-      <div>
-        <button onClick={goXU}>小小徐过去</button>
-      </div>
-    </div>
-  )
-}
 
+  
+
+  Router.events.on('routeChangeStart',(...args)=>{
+    console.log('1.routeChangeStart->路由开始变化,参数为:',...args)
+  })
+
+  Router.events.on('routeChangeComplete',(...args)=>{
+    console.log('2.routeChangeComplete->路由结束变化,参数为:',...args)
+  })
+
+  Router.events.on('beforeHistoryChange',(...args)=>{
+    console.log('3,beforeHistoryChange->在改变浏览器 history之前触发,参数为:',...args)
+  })
+
+  Router.events.on('routeChangeError',(...args)=>{
+    console.log('4,routeChangeError->跳转发生错误,参数为:',...args)
+  })
+ 
+  Router.events.on('hashChangeStart',(...args)=>{
+    console.log('5,hashChangeStart->hash跳转开始时执行,参数为:',...args)
+  })
+
+  Router.events.on('hashChangeComplete',(...args)=>{
+    console.log('6,hashChangeComplete->hash跳转完成时,参数为:',...args)
+  })
+ 
+ 
+  
+
+  return(
+    <>
+      <div>我是首页</div>
+      <div>
+        <Link href={{pathname:'/XUXU',query:{name:'结衣'}}}><a>选结衣</a></Link><br/>
+        <Link href="/XUXU?name=井空"><a>选井空</a></Link>
+      </div>
+      <div>
+        <button onClick={goXUXU}>选井空</button>
+      </div>
+      <div>
+         <Link href="#jspang"><a>选JSPang</a></Link>
+      </div>
+    </>
+  )
+
+}
 export default Home

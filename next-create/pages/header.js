cb145707736f11ce8f5c2fd07efc2b0de160e446
theone <xuguangyue@dtrobo.com>
2019-10-21 15:04:40 +0800

    样式引入以及antd

--- next-create/pages/header.js
+++ next-create/pages/header.js
@@ -1,5 +1,7 @@
 // import Head from 'next/head'
 import MyHeader from '../components/myHeader'
+import '../static/test.css'
+import {Button} from 'antd'
 function Header() {
     return (
         <>
@@ -7,6 +9,7 @@ function Header() {
                 <title>哈哈哈标题</title>
             </Head> */}
             <MyHeader></MyHeader>
+            <div><Button>我是按钮</Button></div>
             <div>啊哈哈哈哈哈</div>
         </>
     )

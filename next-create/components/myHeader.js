42f23ced1b981054d78c511f3a6c3a637f1918f0
theone <xuguangyue@dtrobo.com>
2019-10-21 11:53:32 +0800

    SEO

---
+++ next-create/components/myHeader.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+const myHeader = () => {
+    return (
+        <>
+         <Head>
+             <title>公共title</title>
+         </Head>
+        </>
+    )
+}
+export default myHeader
---
+++ next-create/pages/header.js
@@ -0,0 +1,14 @@
+// import Head from 'next/head'
+import MyHeader from '../components/myHeader'
+function Header() {
+    return (
+        <>
+            {/* <Head>
+                <title>哈哈哈标题</title>
+            </Head> */}
+            <MyHeader></MyHeader>
+            <div>啊哈哈哈哈哈</div>
+        </>
+    )
+}
+export default Header

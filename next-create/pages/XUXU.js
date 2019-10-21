2f16adb247f6287bbe73291e91cac3a78b2e6b88
theone <xuguangyue@dtrobo.com>
2019-10-21 10:50:12 +0800

    axios请求数据

--- next-create/pages/XUXU.js
+++ next-create/pages/XUXU.js
@@ -1,13 +1,27 @@
 import {withRouter} from 'next/router'
 import Link from 'next/link'
+import axios from 'axios'
 
-function XUXU({router}) {
+function XUXU({router,list}) {
     return (
         <>  
             <div>{router.query.name}</div>
+            <div >{list}</div>
             <Link href="/"><a>返回</a></Link>
         </>
     )
 }
+ XUXU.getInitialProps = async ()=>{
+    const promise =new Promise((resolve)=>{
+            axios('https://www.easy-mock.com/mock/5d4b7f412b2be649f7fe3542/reactDemo/menuBar').then(
+                (res)=>{
+                    console.log('远程数据结果：',res)
+                    resolve(res.data.data)
+                }
+            )
+    })
+    return await promise
+}
+
 
 export default withRouter(XUXU)
